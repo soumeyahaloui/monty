654a6467d5d3ee069c1c552671b6b814fa76530b
soumeyahaloui <soumeyahaloui@gmail.com>
2023-06-20 16:53:44 -0700

    updated

--- 0.push.c
+++ 0.push.c
@@ -1,42 +1,37 @@
 #include "monty.h"
-
 /**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
  */
-int main(int argc, char *argv[])
+void f_push(stack_t **head, unsigned int counter)
 {
-	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
+	int n, j = 0, flag = 0;
 
-	if (argc != 2)
+	if (bus.arg)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
-
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
-	}
-
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		line_number++;
-		/* Tokenize the line and process the opcode */
-		/* Your code here */
-	}
-
-	free(line);
-	fclose(file);
-	return (EXIT_SUCCESS);
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
