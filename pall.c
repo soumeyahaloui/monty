ae9108e1f43c88f224868e26265a834c3a28cfaf
soumeyahaloui <soumeyahaloui@gmail.com>
2023-06-20 17:38:01 -0700

    updated

---
+++ pall.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * pall - Prints all values on the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number of the instruction in the file being processed.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+	(void) line_number;
+}
