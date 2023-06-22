9af698fa3fd3bf01dfae4a629dccc12eb1477af5
soumeyahaloui <soumeyahaloui@gmail.com>
2023-06-22 16:17:52 -0700

    updated

--- monty.h
+++ monty.h
@@ -16,6 +16,17 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
+/*
+ * Struct: instruction_t
+ * ---------------------
+ * Description: Represents an instruction in a program.
+ * Members: opcode (char*), f (function pointer)
+ */
+typedef struct instruction_s {
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 /* Function prototypes */
 void get_opcode_func(char *opcode, stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
