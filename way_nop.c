01e5263075853c7ab2676624273b1d32b0faa232
amirakame <amirakamel343@gmail.com>
2023-06-22 16:19:01 -0700

    choll

---
+++ way_nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number of the instruction in the file being processed.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
